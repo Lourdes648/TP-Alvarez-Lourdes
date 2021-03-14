2fcaa973c7d32c0ea8adf5f1427afbbb8a30447e
User <User@DESKTOP-GMMK5OR>
2021-03-14 18:13:28 -0300

    Capas de Usuario y Producto

---
+++ src/main/java/ar/edu/unju/edm/service/imp/ProductoServiceImp.java
@@ -0,0 +1,28 @@
+package ar.edu.unju.edm.service.imp;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ar.edu.unju.edm.model.Producto;
+import ar.edu.unju.edm.repository.IProductoDAO;
+import ar.edu.unju.edm.service.IProductoService;
+
+@Service
+public class ProductoServiceImp implements IProductoService {
+
+	@Autowired
+	IProductoDAO iProductoDAO;
+	@Autowired
+	Producto producto;
+	@Override
+	public void guardarProducto(Producto producto) {
+		iProductoDAO.save(producto);
+	}
+
+	@Override
+	public List<Producto> listarProductos() {
+		return (List<Producto>) iProductoDAO.findAll();
+	}
+
+}
